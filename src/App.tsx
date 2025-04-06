8f310f690a08ec473b74b742512dd98e2d34ef74
Shivraj Roy <shivrajroy78@gmail.com>
2025-04-06 13:11:39 +0530

    feat: implement routing...

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./root/pages";
+import SigninForm from "./auth/form/SigninForm";
+import SignupForm from "./auth/form/SignupForm";
+import AuthLayout from "./auth/AuthLayout";
+import RootLayout from "./root/RootLayout";
+
 function App() {
    return (
-      <>
-         <h1 className="cursor-pointer underline">Instaflick</h1>
-      </>
+      <main className="flex h-screen">
+         <Routes>
+            {/* Public Routes */}
+            <Route element={<AuthLayout />}>
+               <Route path="/sign-in" element={<SigninForm />} />
+               <Route path="/sign-up" element={<SignupForm />} />
+            </Route>
+
+            {/* Private Routes */}
+            <Route element={<RootLayout />}>
+               <Route index element={<Home />} />
+            </Route>
+         </Routes>
+      </main>
    );
 }
 
