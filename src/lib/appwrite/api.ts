21db8a6abe1162b73070a476f2a6229735d90d45
Shivraj Roy <shivrajroy78@gmail.com>
2025-04-07 23:52:57 +0530

    feat: enhance user account creation by adding database and avatar handling...

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,6 @@
 import { ID } from "appwrite";
 import { INewUser } from "@/types";
-import { account } from "./config";
+import { account, appwriteConfig, avatars, databases } from "./config";
 
 export const createUserAccount = async (user: INewUser) => {
    try {
@@ -10,9 +10,42 @@ export const createUserAccount = async (user: INewUser) => {
          user.password,
          user.name
       );
-      return newAccount;
+      if (!newAccount) {
+         throw new Error("Failed to create user account");
+      }
+
+      const avatarUrl = avatars.getInitials(user.name);
+      const newUser = await saveUserToDB({
+         accountId: newAccount.$id,
+         email: user.email,
+         name: user.name,
+         imageUrl: new URL(avatarUrl),
+         username: user.username,
+      });
+      return newUser;
    } catch (error) {
       console.error("Error creating user account: ", error);
       throw new Error("Failed to create user account");
+   }
+};
+
+export const saveUserToDB = async (user: {
+   accountId: string;
+   email: string;
+   name: string;
+   imageUrl: URL;
+   username?: string;
+}) => {
+   try {
+      const newUser = await databases.createDocument(
+         appwriteConfig.databaseId,
+         appwriteConfig.userCollectionId,
+         ID.unique(),
+         user
+      );
+      return newUser;
+   } catch (error) {
+      console.error("Error saving user to database: ", error);
+      throw new Error("Failed to save user to database");
    }
 };
